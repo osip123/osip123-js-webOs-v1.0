80b041742a0c3a4e5764edb7ae84126d94e4dc45
osip123 <131646261+osip123@users.noreply.github.com>
2024-02-23 13:22:43 +0300

    0.24

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { useState } from 'react';
+import './header.scss'
 
 const Header = () => {
+
+    const [open, setOpen] = useState(false);    
+    console.log(open);
+
+    let data = new Date();
+    console.log(data)
+
   return (
-    <div>
-      
-    </div>
+    <header>
+        <div className="wi">
+            <button
+                onClick={e => open? setOpen(false): setOpen(true)}
+            style={{border: 'none', outline: 'none'}}>Обзор</button>
+        </div>
+        <div className="time">
+        </div>
+        <div className="menu">
+            {}
+        </div>
+    </header>
   )
 }
 
